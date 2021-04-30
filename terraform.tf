d5e7e1dd428731b2f01532002adc5e4c01fe5217
JamesJJ <jj@fcg.fyi>
2021-04-30 19:18:03 +0800

    Require TF >= 0.15.1 due to Hashicorp supply chain issue and corresponding signing key change

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13.2"
+  required_version = ">= 0.15.1"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.32.0"
+      version = ">= 3.38.0"
     }
   }
 }
